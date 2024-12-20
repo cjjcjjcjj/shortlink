ea79ebfb9598fae9c573bd9b48082797802bdc33
cjj <1324479996@qq.com>
2024-12-20 14:17:29 +0800

    通过分布式锁完善用户注册功能，防止恶意请求注册

--- admin/src/main/java/com/nageoffer/shortlink/admin/service/Impl/UserServiceImpl.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/Impl/UserServiceImpl.java
@@ -3,9 +3,7 @@ package com.nageoffer.shortlink.admin.service.Impl;
 import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
-import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.nageoffer.shortlink.admin.common.enums.UserErrorCodeEnum;
 import com.nageoffer.shortlink.admin.common.convention.exception.ClientException;
 import com.nageoffer.shortlink.admin.dao.entity.UserDo;
 import com.nageoffer.shortlink.admin.dao.mapper.UserMapper;
@@ -14,8 +12,13 @@ import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
 import com.nageoffer.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RBloomFilter;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+
+import static com.nageoffer.shortlink.admin.common.constant.RedisCacheConstant.*;
+import static com.nageoffer.shortlink.admin.common.enums.UserErrorCodeEnum.*;
 
 /**
  * 用户接口实现层
@@ -26,6 +29,8 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDo> implements UserService {
 
     private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
+    private final RedissonClient redissonClient;
+    //看门狗机制
 
     @Override
     public UserRespDTO getUserByUsername(String username) {
@@ -35,7 +40,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDo> implements 
 //        它的作用是根据 UserDo 类的 getUsername 方法来构建一个 username 字段的 = (equal) 查询条件，username 变量是查询条件的值。
         UserDo userDo = baseMapper.selectOne(queryWrapper);
         if (userDo == null) {
-            throw new ClientException(UserErrorCodeEnum.USER_NULL);
+            throw new ClientException(USER_NULL);
         }
         UserRespDTO result = new UserRespDTO();
         BeanUtils.copyProperties(userDo, result);
@@ -50,13 +55,23 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDo> implements 
     @Override
     public void register(UserRegisterReqDTO userRegisterReqDTO) {
         if (!hasUsername(userRegisterReqDTO.getUsername())){
-            throw new ClientException(UserErrorCodeEnum.USER_NAME_EXIST);
+            throw new ClientException(USER_NAME_EXIST);
         }
-        int inserted = baseMapper.insert(BeanUtil.toBean(userRegisterReqDTO, UserDo.class));
-        if (inserted < 1){
-            throw new ClientException(UserErrorCodeEnum.USER_SAVE_ERROR);
+        RLock lock = redissonClient.getLock(LOCK_USER_REGISTER_KEY + userRegisterReqDTO.getUsername());
+        try {
+            if (lock.tryLock()){
+                //只给一个锁就行，其他就是尝试，默认有一个会成功注册
+                int inserted = baseMapper.insert(BeanUtil.toBean(userRegisterReqDTO, UserDo.class));
+                if (inserted < 1){
+                    throw new ClientException(USER_SAVE_ERROR);
+                }
+                userRegisterCachePenetrationBloomFilter.add(userRegisterReqDTO.getUsername());
+                //存储用户名，以便在后续的用户注册请求中检查该用户名是否已经被注册
+                return;
+            }
+            throw new ClientException(USER_SAVE_ERROR);
+        } finally {
+            lock.unlock();
         }
-        userRegisterCachePenetrationBloomFilter.add(userRegisterReqDTO.getUsername());
-        //存储用户名，以便在后续的用户注册请求中检查该用户名是否已经被注册
     }
 }
