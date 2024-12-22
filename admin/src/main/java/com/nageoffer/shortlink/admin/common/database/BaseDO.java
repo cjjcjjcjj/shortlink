950e4bb1fd444bf3b1361cea76cad8f527d8d586
cjj <1324479996@qq.com>
2024-12-22 16:29:59 +0800

    开发数据库持久层对象基础属性

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/database/BaseDO.java
@@ -0,0 +1,23 @@
+package com.nageoffer.shortlink.admin.common.database;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * 数据库持久层对象基础属性
+ */
+@Data
+public class BaseDO {
+
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Integer delFlag;
+}
