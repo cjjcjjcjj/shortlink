b4de073004ea3f8f8173aa176b8ed293de17591c
cjj <1324479996@qq.com>
2024-12-29 12:13:02 +0800

    用新的依赖来确定操作系统和浏览器

--- project/src/main/java/com/nageoffer/shortlink/project/toolkit/LinkUtil.java
+++ project/src/main/java/com/nageoffer/shortlink/project/toolkit/LinkUtil.java
@@ -2,6 +2,8 @@ package com.nageoffer.shortlink.project.toolkit;
 import cn.hutool.core.date.DateUnit;
 import cn.hutool.core.date.DateUtil;
 import jakarta.servlet.http.HttpServletRequest;
+import ua_parser.Client;
+import ua_parser.Parser;
 
 import java.util.Date;
 import java.util.Optional;
@@ -51,22 +53,23 @@ public class LinkUtil {
      * @param request 请求
      * @return 访问操作系统
      */
-    public static String getOs(HttpServletRequest request) {
+    public static String getOs(HttpServletRequest request){
         String userAgent = request.getHeader("User-Agent");
-        if (userAgent.toLowerCase().contains("windows")) {
-            return "Windows";
-        } else if (userAgent.toLowerCase().contains("mac")) {
-            return "Mac OS";
-        } else if (userAgent.toLowerCase().contains("linux")) {
-            return "Linux";
-        } else if (userAgent.toLowerCase().contains("unix")) {
-            return "Unix";
-        } else if (userAgent.toLowerCase().contains("android")) {
-            return "Android";
-        } else if (userAgent.toLowerCase().contains("iphone")) {
-            return "iOS";
-        } else {
-            return "Unknown";
-        }
+        Parser parser = new Parser();
+        Client client = parser.parse(userAgent);
+        return client.os.family; // 返回操作系统类型
     }
+
+    /**
+     * 解析用户的浏览器并返回
+     * @param request
+     * @return
+     */
+    public static String getBrowser(HttpServletRequest request){
+        String userAgent = request.getHeader("User-Agent");
+        Parser parser = new Parser();
+        Client client = parser.parse(userAgent);
+        return client.userAgent.family; // 返回浏览器类型
+    }
+
 }
